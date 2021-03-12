07a20c636ec5c94da38d22a3171dd7f832ce303c
maverick.820 <60806278+taksh-doria@users.noreply.github.com>
2021-03-12 22:07:18 +0530

    Signout and Ui Enhancement

--- app/src/main/java/com/maverick/findmyfood/appmain/AccountFragment.java
+++ app/src/main/java/com/maverick/findmyfood/appmain/AccountFragment.java
@@ -1,5 +1,6 @@
 package com.maverick.findmyfood.appmain;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.maverick.findmyfood.MainActivity;
 import com.maverick.findmyfood.R;
 import com.maverick.findmyfood.utility.Authentication;
 
@@ -25,6 +29,7 @@ public class AccountFragment extends Fragment {
     private CircleImageView profile_image;
     private TextView display_name;
     private TextView email;
+    private Button signout;
     FirebaseAuth mauth;
     GoogleSignInClient signInClient;
     public AccountFragment() {
@@ -47,11 +52,27 @@ public class AccountFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         root=inflater.inflate(R.layout.fragment_account, container, false);
+        signout=(Button)root.findViewById(R.id.signout);
         profile_image=(CircleImageView) root.findViewById(R.id.profile_pic);
         display_name=(TextView)root.findViewById(R.id.username);
         email=(TextView)root.findViewById(R.id.email);
         mauth=FirebaseAuth.getInstance();
         signInClient= Authentication.requestService(root.getContext());
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                GoogleSignInClient client = Authentication.requestService(root.getContext());
+                client.signOut().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Intent intent = new Intent(root.getContext(), MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // Removes other Activities from stack
+                        startActivity(intent);
+                    }
+                });
+            }
+        });
         return root;
     }
 }
